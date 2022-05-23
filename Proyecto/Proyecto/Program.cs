b40111bd3630100415eab31cfc517f788ea8c2f3
sergio.ramirez <sramirezpch@gmail.com>
2022-05-22 20:38:55 -0500

    Added Login Form with harcoded draft validation

--- Proyecto/Proyecto/Program.cs
+++ Proyecto/Proyecto/Program.cs
@@ -16,7 +16,7 @@ namespace Proyecto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
